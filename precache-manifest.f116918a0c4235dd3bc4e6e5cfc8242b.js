9223adf874d597766f0f580d57536c7c329ae98a
unknown <williambrooks287@gmail.com>
2022-05-11 12:58:57 -0400

    Updates

---
+++ precache-manifest.f116918a0c4235dd3bc4e6e5cfc8242b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc02b95de62e3bbf03431672196ef11",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "54e85f28d53af42762cc",
+    "url": "/react-portfolio/static/css/main.23f2148b.chunk.css"
+  },
+  {
+    "revision": "a13ac4f309b6567caa12",
+    "url": "/react-portfolio/static/js/2.a81a3bfd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-portfolio/static/js/2.a81a3bfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e85f28d53af42762cc",
+    "url": "/react-portfolio/static/js/main.1d29ee05.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "550338999935d402b7497fd1dd14e88d",
+    "url": "/react-portfolio/static/media/book-search-img.55033899.PNG"
+  },
+  {
+    "revision": "311bf482534f4858693b84a0dcffb111",
+    "url": "/react-portfolio/static/media/budget-img.311bf482.PNG"
+  },
+  {
+    "revision": "e6d2fb4d2daa859fba29ab6117c6cce5",
+    "url": "/react-portfolio/static/media/cod-connect-img.e6d2fb4d.png"
+  },
+  {
+    "revision": "545f5e0765bc3730f69dbd4ca4d2e529",
+    "url": "/react-portfolio/static/media/password-img.545f5e07.PNG"
+  },
+  {
+    "revision": "0fb10b862dd4c39fef54e3e7c496f3a9",
+    "url": "/react-portfolio/static/media/picture-me.0fb10b86.PNG"
+  },
+  {
+    "revision": "033ba347dcc49f63b37a6532f254fb5a",
+    "url": "/react-portfolio/static/media/tabletop-img.033ba347.PNG"
+  },
+  {
+    "revision": "e15e959eb4eb9bd1a5bf11aee7b4841d",
+    "url": "/react-portfolio/static/media/task-img.e15e959e.PNG"
+  }
+]);
