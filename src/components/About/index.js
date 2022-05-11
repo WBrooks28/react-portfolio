316873f4e0ef692350d48fb261a8f76d5e6e8c10
unknown <williambrooks287@gmail.com>
2022-05-11 10:16:26 -0400

    add about me, image not displaying

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function About() {
     return (
-        <section>
-            <h1>About Me</h1>
+        <section className="text-center">
+            <h1 className="text-secondary">About Me</h1>
+            <img src="../../assets/picture-me.PNG" alt="" className="img-fluid"></img>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </section>
     )
 }
