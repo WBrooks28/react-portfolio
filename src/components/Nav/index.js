284bb3ced8461141a6e9f0b553fb092e272e4bc1
unknown <williambrooks287@gmail.com>
2022-05-11 00:55:11 -0400

    add header

---
+++ src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav() {
+    return(
+        <div className="navbar bg-light fixed-top justify-content-center">
+            <ul className="nav m-2">
+                <li className="nav-item m-2">William Brooks</li>
+                <li className="nav-item m-2">About Me</li>
+                <li className="nav-item m-2">Portfolio</li>
+                <li className="nav-item m-2">Contact</li>
+                <li className="nav-item m-2">Resume</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
