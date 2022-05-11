e9dd411e4cf40eb4c048b92418100a60f702cc49
unknown <williambrooks287@gmail.com>
2022-05-11 11:13:18 -0400

    add resume page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,8 @@ function Footer() {
     return(
         <footer className="flex-row justify-content-around p-3">
             <a href="https://github.com/WBrooks28" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a href="https://www.linkedin.com/in/william-brooks28/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                <a href="mailto:williambrooks287@gmail.com">williambrooks287@gmail.com</a>
+            <a href="https://www.linkedin.com/in/william-brooks28/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="mailto:williambrooks287@gmail.com">williambrooks287@gmail.com</a>
         </footer>
     );
 }
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function Resume() {
     return (
-        <section>
-            <h1>Resume</h1>
+        <section className="text-center">
+            <h1 className="text-secondary mb-5">Resume</h1>
+            <a href="https://docs.google.com/document/d/10MVGdDkQG1QYTYPxcRKDzHhFRCu2b02Wxs8gjrr4dNs/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+                View and Download Here
+            </a>
+            <div className="m-5"></div>
         </section>
     )
 }
