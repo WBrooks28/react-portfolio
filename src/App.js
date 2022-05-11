2f2d87bd0108c72b108239b514bb0bebe09cb747
unknown <williambrooks287@gmail.com>
2022-05-11 01:27:07 -0400

    setting up routes, having issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
 
 function App() {
   return (
-        <Header />
+        <Router>
+          <Header />
+          <Routes>
+            <Route exact path='/' component={About} />
+            <Route exact path='/portfolio' component={Portfolio} />
+            <Route exact path='/resume' component={Resume} />
+            <Route exact path='/contact' component={Contact} />
+          </Routes>
+        </Router>
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function Nav() {
     return(
         <div className="navbar bg-light fixed-top justify-content-center">
             <ul className="nav m-2">
                 <li className="nav-item m-2">William Brooks</li>
-                <li className="nav-item m-2">About Me</li>
-                <li className="nav-item m-2">Portfolio</li>
-                <li className="nav-item m-2">Contact</li>
-                <li className="nav-item m-2">Resume</li>
+                <li className="nav-item m-2">
+                    <Link to='/'>About me</Link>
+                </li>
+                <li className="nav-item m-2">
+                    <Link to='/portfolio'>Portfolio</Link>
+                </li>
+                <li className="nav-item m-2">
+                    <Link to='/resume'>Resume</Link>
+                </li>
+                <li className="nav-item m-2">
+                    <Link to='/contact'>Contact</Link>
+                </li>
             </ul>
         </div>
     )
